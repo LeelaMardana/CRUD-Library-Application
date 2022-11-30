8bd6174904f1a8a59adeaf37efcd5d3b5e2f5a79
AwesemoTowa <Deankurumi@gmail.com>
2022-11-30 17:46:03 +0600

    Add axios

--- client/src/api.js
+++ client/src/api.js
@@ -1,73 +1,137 @@
+import axios from 'axios';
+//API with React Query
+
+// export const getAllBooks = async () => {
+//   const response = await fetch(`${process.env.REACT_APP_API_SERVER}/books`);
+
+//   if (!response.ok) {
+//     throw new Error('Something went wrong.');
+//   }
+
+//   return response.json();
+// };
+
+// export const getBook = async ({ queryKey }) => {
+//   /* eslint-disable no-unused-vars */
+//   const [_key, { id }] = queryKey;
+//   const response = await fetch(
+//     `${process.env.REACT_APP_API_SERVER}/books/${id}`
+//   );
+
+//   if (!response.ok) {
+//     throw new Error(response.json().message);
+//   }
+
+//   return response.json();
+// };
+
+// export const updateBook = async ({ id, ...data }) => {
+//   const response = await fetch(
+//     `${process.env.REACT_APP_API_SERVER}/books/${id}`,
+//     {
+//       method: 'PUT',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(data),
+//     }
+//   );
+
+//   if (!response.ok) {
+//     throw new Error(response.json().message);
+//   }
+
+//   return response.json();
+// };
+
+// export const createBook = async ({ ...data }) => {
+//   const response = await fetch(`${process.env.REACT_APP_API_SERVER}/books/`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//   });
+
+//   if (!response.ok) {
+//     throw new Error(response.json().message);
+//   }
+
+//   return response.json();
+// };
+
+// export const removeBook = async id => {
+//   const response = await fetch(
+//     `${process.env.REACT_APP_API_SERVER}/books/${id}`,
+//     {
+//       method: 'DELETE',
+//     }
+//   );
+
+//   if (!response.ok) {
+//     throw new Error(response.json().message);
+//   }
+
+//   return true;
+// };
+
+// Получить книги
+
+// API with Axios
 export const getAllBooks = async () => {
-  const response = await fetch(`${process.env.REACT_APP_API_SERVER}/books`);
-
-  if (!response.ok) {
-    throw new Error('Something went wrong.');
+  try {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_SERVER}/books`
+    );
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
   }
-
-  return response.json();
 };
 
 export const getBook = async ({ queryKey }) => {
   /* eslint-disable no-unused-vars */
   const [_key, { id }] = queryKey;
-  const response = await fetch(
-    `${process.env.REACT_APP_API_SERVER}/books/${id}`
-  );
-
-  if (!response.ok) {
-    throw new Error(response.json().message);
+  try {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_SERVER}/books/${id}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
-
-  return response.json();
 };
 
 export const updateBook = async ({ id, ...data }) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_SERVER}/books/${id}`,
-    {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    }
-  );
-
-  if (!response.ok) {
-    throw new Error(response.json().message);
+  try {
+    const response = await axios.put(
+      `${process.env.REACT_APP_API_SERVER}/books/${id}`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
-
-  return response.json();
-};
-
-export const createBook = async ({ ...data }) => {
-  const response = await fetch(`${process.env.REACT_APP_API_SERVER}/books/`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-
-  if (!response.ok) {
-    throw new Error(response.json().message);
-  }
-
-  return response.json();
 };
 
 export const removeBook = async id => {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_SERVER}/books/${id}`,
-    {
-      method: 'DELETE',
-    }
-  );
-
-  if (!response.ok) {
-    throw new Error(response.json().message);
+  try {
+    await axios.delete(`${process.env.REACT_APP_API_SERVER}/books/${id}`);
+    return true;
+  } catch (error) {
+    console.error(error);
   }
+};
 
-  return true;
+export const createBook = async data => {
+  try {
+    const response = await axios.post(
+      `${process.env.REACT_APP_API_SERVER}/books/`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
